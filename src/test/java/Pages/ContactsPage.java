ca768b30bbd9450b773edc0dff11ef55c16f4460
rageULV <esawein5@gmail.com>
2024-03-09 23:31:12 +0200

    09.03.24
    lesson 6-2

--- src/test/java/Pages/ContactsPage.java
+++ src/test/java/Pages/ContactsPage.java
@@ -1,10 +1,16 @@
 package Pages;
 
+import model.Contact;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ContactsPage extends BasePage{
     @FindBy(xpath = "//button[contains(text(),'Sign Out')]")
@@ -13,4 +19,55 @@ public class ContactsPage extends BasePage{
         setDriver(driver);
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, 20),this);
     }
+    public boolean getDataFromContactList(Contact contact){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Создается объект класса WebDriverWait,
+        // который ожидает видимости элемента на странице в течение 5 секунд.
+        WebElement nameInContact = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(
+                        By.xpath("//h2[contains(text(),'"+contact.getName().toString()+"')]"))); // Ожидается появление элемента с именем контакта, переданного в методе, используя XPath.
+        nameInContact.click();
+
+        WebElement editButton = driver.findElement(By.xpath("//button[contains(text(),'Edit')]"));
+        editButton.click();
+
+// находим элементы веб-страницы для каждого поля контакта (имя, фамилия, телефон, email, адрес и описание) и получаем их значения с помощью метода getAttribute("value").
+        WebElement elementName = driver.findElement(By.xpath("//input[@placeholder='Name']"));
+        String elementNameValue = elementName.getAttribute("value");
+
+        WebElement elementLastName = driver.findElement(By.xpath("//input[@placeholder='Last Name']"));
+        String elementLastNameValue = elementLastName.getAttribute("value");
+
+        WebElement elementPhone = driver.findElement(By.xpath("//input[@placeholder='Phone']"));
+        String elementPhoneValue = elementPhone.getAttribute("value");
+
+        WebElement elementEmail = driver.findElement(By.xpath("//input[@placeholder='email']"));
+        String elementEmailValue = elementEmail.getAttribute("value");
+
+        WebElement elementAddress = driver.findElement(By.xpath("//input[@placeholder='Address']"));
+        String elementAddressValue = elementAddress.getAttribute("value");
+
+        WebElement elementDescription = driver.findElement(By.xpath("//input[@placeholder='desc']"));
+        String elementDescriptionValue = elementDescription.getAttribute("value");
+
+
+        // Создается новый объект Contact, в который записываются полученные значения полей контакта.
+        Contact listContact = new Contact();
+
+        listContact.setName(elementNameValue);
+        listContact.setLastname(elementLastNameValue);
+        listContact.setPhone(elementPhoneValue);
+        listContact.setEmail(elementEmailValue);
+        listContact.setAddress(elementAddressValue);
+        listContact.setDescription(elementDescriptionValue);
+
+        boolean result = listContact.equals(contact);
+        return result;
+    }
+
+
+
+
+
+
+
 }
