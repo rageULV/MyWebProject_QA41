64ba415f646d1d7e6c234565d366733d015ea224
rageULV <esawein5@gmail.com>
2024-03-14 19:24:07 +0200

    14.03.24
    lesson 7

--- src/test/java/Helpers/terminal_usage.java
+++ src/test/java/Helpers/terminal_usage.java
@@ -2,9 +2,11 @@ package Helpers;
 
 public class terminal_usage {
 
-    //allure generate allure-results --clean -o allure-report
+    //allure generate allure-results --clean -o allure-report (its with cleaning cash of old reports)
+    //allure serve
+    //allure generate allure-results -o allure-report (this one not cleaning and saves all test reports)
 
-
+    
 
 
 
