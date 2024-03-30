3b50cd3e0f1b27440542f6c2940351038665ca6b
rageULV <esawein5@gmail.com>
2024-03-30 19:09:51 +0300

    ss
    ss

--- src/test/java/examples/gitCommands.java
+++ src/test/java/examples/gitCommands.java
@@ -9,4 +9,23 @@ public class gitCommands {
     // git commit -m "my commit MyBranch"   - commits the add command
 
     // git checkout main
-}
+    // git merge mybranch                   - merges the branch you are right now with other you want
+    //                                      in this example I did merge main with mybranch to transfer from mybranch
+    //                                      all I wrote to main
+    //
+    // ALT+0                                - opens window with changes. if you have conflicts while merge,
+    //                                      - there will be "option" resolve there you can commit yours or theirs. (ass I understand no one doing it)
+    //                                      - !!!!!!!!!!!!!!and there will be one more this MERGE(blue button)!!!!!!!!!!!!
+    //                                      - if you press it will open windows with explain where is conflict
+    //                                      - in that windows you have options like: '>>' meaning accept (code from somewhere that conflict with you)
+    //                                      - and there will be                       'x' meaning keep your code.
+
+    //                                      - after solving the conflicts, will pop up "Save and finish" and then you can press apply button
+    // if you have done with conflicts
+    // you can try commit again should work.
+
+
+
+
+
+ }
